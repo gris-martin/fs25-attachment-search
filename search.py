649c90c03d99e3545003fd767d28b21987857d81
Martin Törnqvist <gris_martin@hotmail.com>
2025-12-23 23:31:19 +0100

    Improve output a bit

--- search.py
+++ search.py
@@ -14,7 +14,6 @@ class Vehicle:
     def get_full_name(self):
         return f"{self.brand} {self.name}"
 
-
 def get_attacher_joints_from_xml_node(vehicle_root_node):
     attacher_joints = vehicle_root_node.find("attacherJoints")
     if attacher_joints is None:
@@ -72,27 +71,57 @@ def parse_vehicle_xmls(file_paths):
         print(f"Parsed {current_index}/{total_files} files.", end="\r")
     return vehicles
 
+def get_matching_attachment(vehicle, other_vehicle):
+    for input_att in other_vehicle.input_attachments:
+        if input_att in vehicle.attachments:
+            return input_att
+    return None
+
 def find_attachments_matching_input_attachments(vehicle_list, vehicle):
-    matching_vehicles = []
+    matching_vehicles_by_attachment = {}
+
     for other_vehicle in vehicle_list:
-        for attachment in other_vehicle.attachments:
-            if attachment in vehicle.input_attachments:
-                matching_vehicles.append((other_vehicle, attachment))
-    return matching_vehicles
+        matching_attachment = get_matching_attachment(other_vehicle, vehicle)
+
+        if matching_attachment is None:
+            continue
+
+        if matching_attachment not in matching_vehicles_by_attachment:
+            matching_vehicles_by_attachment[matching_attachment] = []
+
+        matching_vehicles_by_attachment[matching_attachment].append(other_vehicle)
+
+    return matching_vehicles_by_attachment
 
 def find_input_attachments_matching_attachments(vehicle_list, vehicle):
-    matching_vehicles = []
+    matching_vehicles_by_input_attachment = {}
+
     for other_vehicle in vehicle_list:
-        for input_att in other_vehicle.input_attachments:
-            if input_att in vehicle.attachments:
-                matching_vehicles.append((other_vehicle, input_att))
-    return matching_vehicles
+        matching_attachment = get_matching_attachment(vehicle, other_vehicle)
+
+        if matching_attachment is None:
+            continue
+
+        if matching_attachment not in matching_vehicles_by_input_attachment:
+            matching_vehicles_by_input_attachment[matching_attachment] = []
+
+        matching_vehicles_by_input_attachment[matching_attachment].append(other_vehicle)
+
+    return matching_vehicles_by_input_attachment
 
 def find_vehicle_by_full_name(vehicle_list, full_name):
     for vehicle in vehicle_list:
         if vehicle.get_full_name() == full_name:
             return vehicle
     return None
+
+def create_map_of_vehicles_by_type(vehicle_list):
+    vehicle_map = {}
+    for vehicle in vehicle_list:
+        if vehicle.type not in vehicle_map:
+            vehicle_map[vehicle.type] = []
+        vehicle_map[vehicle.type].append(vehicle)
+    return vehicle_map
 
 def print_vehicle_info(vehicle):
     print(f"Vehicle: {vehicle.brand} {vehicle.name}")
@@ -103,14 +132,20 @@ def print_vehicle_info(vehicle):
     for input_att in vehicle.input_attachments:
         print(f"  Input Attachment Point: {input_att}")
 
-def print_short_vehicle_info(vehicle):
-    print(f"{vehicle.get_full_name()} - Type: {vehicle.type}, Store Category: {vehicle.store_category}")
+def get_short_vehicle_info(vehicle):
+    return f"{vehicle.get_full_name()} - Type: {vehicle.type}, Store Category: {vehicle.store_category}"
 
 def main():
     # Parse command line arguments for vehicle name
-    vehicle_name = "LIZARD Pickup 2017"  # Placeholder for command line argument
+    # vehicle_name = "LIZARD Pickup 2017"  # Placeholder for command line argument
+    # vehicle_name = "CASEIH Axial-Flow 7150"  # Placeholder for command line argument
+    # vehicle_name = "POETTINGER Terradisc 10001T"  # Placeholder for command line argument
+    # vehicle_name = "LIZARD S-710"  # Placeholder for command line argument
+    vehicle_name = "PFANZELT DW P 186"  # Placeholder for command line argument
+    
     # vehicles_directory = Path("./vehicles")  # Placeholder for vehicles directory
-    vehicles_directory = Path(r"C:\Program Files (x86)\Steam\steamapps\common\Farming Simulator 25\data\vehicles")  # Placeholder for vehicles directory
+    # vehicles_directory = Path(r"C:\Program Files (x86)\Steam\steamapps\common\Farming Simulator 25\data\vehicles")  # Placeholder for vehicles directory
+    vehicles_directory = Path(r"Z:\Steam\steamapps\common\Farming Simulator 25\data\vehicles")  # Placeholder for vehicles directory
 
     # Recursively get a list of all xml files in a directory specified from command line arguments
     xml_files = []
@@ -131,16 +166,24 @@ def main():
     print_vehicle_info(vehicle)
 
     # Find other vehicles that can attach to these points
+    print(f"\nVehicles that can attach to '{vehicle.get_full_name()}':")
     matching_vehicles = find_attachments_matching_input_attachments(vehicle_list, vehicle)
-    print(f"\nVehicles that can attach to2 '{vehicle.get_full_name()}':")
-    for match, att in matching_vehicles:
-        print_short_vehicle_info(match)
+    print_matches(matching_vehicles)
 
     # Find other vehicles that this vehicle can attach to
-    matching_vehicles = find_input_attachments_matching_attachments(vehicle_list, vehicle)
     print(f"\nVehicles that '{vehicle.get_full_name()}' can attach to:")
-    for match, input_att in matching_vehicles:
-        print_short_vehicle_info(match)
+    matching_vehicles = find_input_attachments_matching_attachments(vehicle_list, vehicle)
+    print_matches(matching_vehicles)
+
+def print_matches(matching_vehicles):
+    for attachment in matching_vehicles:
+        print(f"Attachment Point: {attachment}")
+        matching_vehicles_by_type = create_map_of_vehicles_by_type(matching_vehicles[attachment])
+        for type in matching_vehicles_by_type:
+            print(f"  Type: {type}")
+            for match in matching_vehicles_by_type[type]:
+                print(f'    {get_short_vehicle_info(match)}')
+            print()
 
 if __name__ == "__main__":
     main()
